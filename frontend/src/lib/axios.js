f610f6e7785d62a515cee21e68e03d9861c69d8d
Dushani Ekanayake <ekanayakeemdk@students.nsbm.ac.lk>
2026-06-25 17:37:36 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,5 @@
 import axios from "axios";
-
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:5001/api"
-      : "https://chatz-real-time-chat-app.onrender.com/api",
-  withCredentials: true,
+baseURL: import.meta.env.MODE === "development" ? "[http://localhost:5001/api](http://localhost:5001/api)" : "/api",
+withCredentials: true,
 });

6be6b5edc24897f77fbac9b2c000c66fbcb64edc
Dushani Ekanayake <ekanayakeemdk@students.nsbm.ac.lk>
2026-07-01 17:54:59 +0530

    Update axios baseURL for production environment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,9 @@
 import axios from "axios";
+
 export const axiosInstance = axios.create({
-baseURL: import.meta.env.MODE === "development" ? "[http://localhost:5001/api](http://localhost:5001/api)" : "/api",
-withCredentials: true,
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api"
+      : "https://chatz-real-time-chat-app-ifoa.vercel.app/api",
+  withCredentials: true,
 });
